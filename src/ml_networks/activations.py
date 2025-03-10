1377acd8a301e7573084e804a0f96d8138b6daab
oakwood-fujiken <oakwood.n14.4sp@keio.jp>
2025-03-10 11:48:11 +0900

    docstring追加&doctest pass&CRReLU追加

--- src/ml_networks/activations.py
+++ src/ml_networks/activations.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 
 class Activation(nn.Module):
-    def __init__(self, activation: str, reparametarize_fn: str = "gelu", **kwargs):
+    def __init__(self, activation: str, **kwargs):
         super().__init__()
         if "glu" not in activation.lower():
             kwargs.pop("dim", None)
@@ -14,9 +14,11 @@ class Activation(nn.Module):
             if activation == "TanhExp":
                 self.activation = TanhExp()
             elif activation == "REReLU":
-                self.activation = REReLU(reparametarize_fn)
-            elif activation == "SiGLU":
+                self.activation = REReLU(**kwargs)
+            elif activation == "SiGLU" or activation == "SwiGLU":
                 self.activation = SiGLU(**kwargs)
+            elif activation == "CRReLU":
+                self.activation = CRReLU(**kwargs)
             else:
                 raise NotImplementedError(
                     f"Activation: '{activation}' is not implemented yet."
@@ -28,6 +30,27 @@ class Activation(nn.Module):
 class REReLU(nn.Module):
     def __init__(self, reparametarize_fn: str = "gelu") -> None:
         super().__init__()
+        """
+        Reparametarized ReLU activation function. This backward pass is differentiable.
+
+        References
+        ----------
+        https://openreview.net/forum?id=lNCnZwcH5Z
+        
+        Parameters:
+        ----------
+        reparametarize_fn : str
+            Reparametarization function. Default is GELU.
+
+        Examples:
+        ---------
+        >>> rerelu = REReLU()
+        >>> x = torch.randn(1, 3)
+        >>> output = rerelu(x)
+        >>> output.shape
+        torch.Size([1, 3])
+
+        """
         reparametarize_fn = reparametarize_fn.lower()
         self.reparametarize_fn = getattr(F, reparametarize_fn)
 
@@ -38,10 +61,61 @@ class REReLU(nn.Module):
             - self.reparametarize_fn(x).detach()
         )
 
+class CRReLU(nn.Module):
+    def __init__(self, lr: float = 0.01) -> None:
+        """
+        Correction Regularized ReLU activation function. This is a variant of ReLU activation function.
+
+        References
+        ----------
+        https://openreview.net/forum?id=7TZYM6Hm9p
+
+        Parameters:
+        ----------
+        lr : float
+            Learning rate. Default is 0.01.
+
+        Examples:
+        ---------
+        >>> crrelu = CRReLU()
+        >>> x = torch.randn(1, 3)
+        >>> output = crrelu(x)
+        >>> output.shape
+        torch.Size([1, 3])
+        """
+        super().__init__()
+        self.lr = nn.Parameter(torch.tensor(lr).float())
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return F.relu(x) + self.lr * x * torch.exp(-(x**2)/2)
+
 class SiGLU(nn.Module):
     def __init__(self, dim: int = -1) -> None:
         """
         SiGLU activation function. This is equivalent to SwiGLU (Swish variant of Gated Linear Unit) activation function.
+
+        References
+        ----------
+        https://arxiv.org/abs/2102.11972
+
+        Parameters:
+        ----------
+        dim : int
+            Dimension to split the tensor. Default is -1.
+
+        Examples:
+        ---------
+        >>> siglu = SiGLU()
+        >>> x = torch.randn(1, 4)
+        >>> output = siglu(x)
+        >>> output.shape
+        torch.Size([1, 2])
+
+        >>> siglu = SiGLU(dim=0)
+        >>> x = torch.randn(4, 1)
+        >>> output = siglu(x)
+        >>> output.shape
+        torch.Size([2, 1])
         """
         super().__init__()
         self.dim = dim
@@ -54,6 +128,14 @@ class TanhExp(nn.Module):
     def __init__(self) -> None:
         """
         TanhExp activation function.
+
+        Examples:
+        ---------
+        >>> tanhexp = TanhExp()
+        >>> x = torch.randn(1, 3)
+        >>> output = tanhexp(x)
+        >>> output.shape
+        torch.Size([1, 3])
         """
         super().__init__()
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -83,3 +165,6 @@ class TanhExpBase(torch.autograd.Function):
         )
         return grad_input
 
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
