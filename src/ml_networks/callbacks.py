508005e7ad5310d930554bfc1edb808bd31bb676
oakwood-fujiken <oakwood.n14.4sp@keio.jp>
2025-05-14 18:56:02 +0900

    future annotationで対応

--- src/ml_networks/callbacks.py
+++ src/ml_networks/callbacks.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from collections.abc import Iterable
 
 import pytorch_lightning as pl
--- src/ml_networks/distributions.py
+++ src/ml_networks/distributions.py
@@ -1,3 +1,5 @@
+
+from __future__ import annotations
 from typing import Literal, Union, Tuple
 import os
 
